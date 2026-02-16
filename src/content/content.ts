0f9efba375a02164b0d677a92fd57a550cca126f
Sameer <akita.sameer1@gmail.com>
2026-02-16 15:28:16 +0900

    refactor content file

--- src/content/content.ts
+++
@@ -1,5 +0,0 @@
-import { setupActiveFieldTracking } from "./textbox";
-import { setupTone } from "./tone";
-
-setupActiveFieldTracking();
-setupTone();
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "dist",
     rollupOptions: {
       input: {
-        content: "src/content/content.ts"
+        content: "src/content/content.tsx"
       },
       output: {
         entryFileNames: "[name].js"
