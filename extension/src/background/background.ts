d780e030e0fbc7fcc1dca42fa9b34f91491fbc5c
Sameer <akita.sameer1@gmail.com>
2026-02-19 23:29:28 +0900

    add chrome types, add background api call

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,1 +1,32 @@
 console.log("background service")
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.type !== "REWRITE") return;
+
+    console.log("Rewrite request received: msg");
+
+    async () => {
+        try {
+            const res = await fetch("http://locahhost:3000/api/rewrite", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    text: msg.text,
+                    tone: msg.tone,
+                }),
+            });
+
+            const data = await res.json();
+
+            console.log("backend response: ", data);
+            sendResponse(data);
+        } catch (err) {
+            console.log("rewrite failed: ", err);
+            sendResponse({ error: "Backend call failed" })
+        }
+    };
+
+    return true;
+})
