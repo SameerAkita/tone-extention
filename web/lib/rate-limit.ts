4cdfab66c5d3e6b8b58a5b79f9785d9f1c1f8c3d
Sameer <akita.sameer1@gmail.com>
2026-03-26 22:55:03 +0900

    fix imports

--- web/lib/rate-limit.ts
+++ web/lib/rate-limit.ts
@@ -1,10 +1,9 @@
 import { Redis } from "@upstash/redis";
-import { Analytics, Ratelimit, RateLimit } from "@upstash/ratelimit";
-import { RateLimitError } from "openai";
+import { Ratelimit } from "@upstash/ratelimit";
 
 const redis = Redis.fromEnv();
 
-export const rewriteRateLimit = new RateLimit({
+export const rewriteRateLimit = new Ratelimit({
     redis,
     limiter: Ratelimit.slidingWindow(10, "10 s"),
     analytics: true,
